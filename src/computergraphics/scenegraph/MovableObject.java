435a0d8a8f815f99a5de2cbcfae41cceaded5523
Christian Schirin <Christian.Schirin@haw-hamburg.de>
2014-11-28 12:36:26 +0100

    Aufgabe 1: MovableObject Skelettimpl. mit konstruktor erstellt

---
+++ src/computergraphics/scenegraph/MovableObject.java
@@ -0,0 +1,66 @@
+/**
+ * Praktikum WPCG, WS 14/15, Gruppe 2
+ * Gruppe: Andreas Mauritz (andreas.mauritz@haw-hamburg.de)
+ *         Christian Schirin (christian.schirin@haw-hamburg.de)
+ * Aufgabe: Aufgabenblatt 5, Aufgabe 1
+ * Verwendete Quellen:
+ *  
+ */
+package computergraphics.scenegraph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.media.opengl.GL2;
+
+import computergraphics.math.Vector3;
+
+/** Diese Klasse ermöglicht es, bewegte Objekte im Szenengraph darzustellen.*/
+public class MovableObject extends Node {
+    
+    public MovableObject(Node geometryNode,Vector3 scale, Vector3 rotAxis, 
+            float rotAngle, List<Vector3> waypoints) {
+        /* Den Objektgraphen aus geometryNode, scaleNode, rotationNode, 
+           translationNode und this zusammenbauen */
+        this.geometryNode = geometryNode;
+        
+        scaleNode = new ScaleNode(scale);
+        scaleNode.addChild(this.geometryNode);
+        
+        rotationNode = new RotationNode(rotAxis, rotAngle);
+        rotationNode.addChild(scaleNode);
+        
+        translationNode = new TranslationNode(new Vector3(0,0,0));
+        translationNode.addChild(rotationNode);
+        
+        addChild(translationNode);
+    }
+    /** "Er beinhaltet auf unterster Ebene die Geometrie des Objektes." */
+    Node geometryNode; //TriangleMeshNode,SingleTriangleNode,etc.
+    
+    /** "Darüber benötigt er einen Skaliserungsknoten, falls die Geometrie 
+     * verformt werden soll." */
+    ScaleNode scaleNode;
+    
+    /** "Darüber hat er einen Rotationsknoten." */
+    RotationNode rotationNode;
+    
+    /** "An oberster Stelle wird ein Translationsknoten benötigt." */
+    TranslationNode translationNode;
+    
+    /** "[...] Speichern Sie daher in jedem MovableObject eine Liste von 
+     * Wegpunkten (jeweils als Vector3 ab)." */
+    List<Vector3> waypoints = new ArrayList<>();
+    
+    /** "Damit Bewegung in die Szene kommt, muss sich der Wert von alpha zur
+     * Laufzeit verändern. Implementieren Sie dazu in der Klasse MoveableObjects
+     */
+    public void tick() {
+        
+    }
+
+    @Override
+    public void drawGl(GL2 gl) {
+        translationNode.drawGl(gl);
+    }
+}
